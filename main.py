46d424d30b6c86875fcbc45fb929106e232095bf
SKAI-24 <70442822+SKAI-24@users.noreply.github.com>
2020-11-15 18:53:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == 757898124319785002:
+        guild = client.get_guild(payload.guild_id)
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            member = guild.get_member(payload.user_id)
+            if member is not None:
+                await member.add_roles(role)
+                print("done")
+            else:
+                print("member not found")
+        else:
+            print("role not found")
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == 757898124319785002:
+        guild = client.get_guild(payload.guild_id)
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            member = guild.get_member(payload.user_id)
+            if member is not None:
+                await member.remove_roles(role)
+                print("done")
+            else:
+                print("member not found")
+        else:
+            print("role not found")
+			
+
+client.run('TOKEN ID')

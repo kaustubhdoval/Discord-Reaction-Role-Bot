0190083361aa9b07d84c071d1c3a75bb111fe6a1
SKAI <70442822+SKAI-24@users.noreply.github.com>
2021-05-24 17:14:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ async def on_message(message):
 @client.event
 async def on_raw_reaction_add(payload):
     if payload.message_id == 757898124319785002:
+	#ENTER THE MESSAGE ID OF THE MESSAGE THAT HAS TO BE REACTED TO
+	
         guild = client.get_guild(payload.guild_id)
         role = discord.utils.get(guild.roles, name=payload.emoji.name)
 
@@ -32,7 +34,7 @@ async def on_raw_reaction_remove(payload):
         if role is not None:
             member = guild.get_member(payload.user_id)
             if member is not None:
-                await member.remove_roles(role)
+                await member.remove_roles(role) 
                 print("done")
             else:
                 print("member not found")
@@ -41,3 +43,4 @@ async def on_raw_reaction_remove(payload):
 			
 
 client.run('TOKEN ID')
+#ENTER YOUR OWN TOKEN ID
